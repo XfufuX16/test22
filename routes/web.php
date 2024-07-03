10d0e8b2ac7e6d4c8ebcb1a66c51b4b1a2a194df
XfufuX16 <evananggie16@outlook.com>
2024-07-03 17:47:21 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route resource for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
+
+//route resource for users
